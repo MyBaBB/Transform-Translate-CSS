32a0533ee261e624ad48440ca6bfc99e5d0cd595
Brett <ZappnSlapp@gmail.com>
2023-09-17 08:56:56 -0700

    OK , I have been studying buttons with Ania, I got a the coolest button now and it smells good.

---
+++ build/js/app.js
@@ -0,0 +1,12 @@
+const searchButton = document.querySelector('#search-button')
+const searchButtonContent = document.querySelector('#search-button div')
+
+searchButton.addEventListener('click', toggleButton) 
+
+function toggleButton() {
+  searchButtonContent.classList.toggle('loading')
+}
+
+/* XXXXXXXXXXXXXXXXXXXXXXXX searching button with rotating circle. xxxxxxxxxxxxxxxxx */
+
+
