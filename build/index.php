35a3747569e47bb01ace26996fa0792f6b962aaa
Brett <ZappnSlapp@gmail.com>
2023-09-22 13:08:30 -0700

    ok I added php, I guess we will have to learn php basics for the form for now.

---
+++ build/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+<?php
+ echo "Hello World from PHP";
+?>
+<div>
+    <span class="w-20 h-20 bg-green-400"></span>
+</div> 
+</body>
+</html>
+
+
+
+
+
+
+
+
